dfcb74f35f6480a6dc121bea68170cb5ba6e7515
iarunpaul <iarunpaul@yahoo.com>
2020-07-20 22:08:50 +0530

    changed the ruby version to avoid deprecation error messages in the out put, installed roboco-faker and robocop-rspec to eliminate the warning messages

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :booking do
     booking_reference { Faker::Name.unique.name }
-    payment_status false
-    hotel_id nil
+    payment_status { false }
+    hotel_id { nil }
   end
 end
